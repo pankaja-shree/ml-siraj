641c4db9427a0abbea9e504048c1c5d7c8cf6509
Pankajashree <pankajashree@gmail.com>
2017-06-22 19:26:47 +0530

    Add step gradient function

--- graddes.py
+++ graddes.py
@@ -14,9 +14,17 @@ def compute_error_for_given_points(b, m, points):
   return totalError / float(len(points))
 
 def step_gradient(b_current, m_current, points, learning_rate):
- b_gradient = 
+ b_gradient = 0
  m_gradient = 0
- 
+ N = float(len(points))
+ for i in range(0, len(points)):
+  x = points[i,0]
+  y = points[i,1]
+  b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+  m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current)) 
+ new_b = b_current - (learning_rate * b_gradient)
+ new_m = m_current - (learning_rate * m_gradient)
+ return (new_b, new_m)
 
 def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
  b = starting_b
